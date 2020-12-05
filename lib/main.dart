4deee4894deba01c9502bfdcf8cde5542cad6aec
Nikhil Nishad <nikhilnishad279@gmail.com>
2020-12-06 03:07:39 +0530

    fuctionality added for playing sound on tap
    Signed-off-by: Nikhil Nishad <nikhilnishad279@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int counter) {
+    final player = AudioCache();
+    player.play('note$counter.wav');
+  }
+
+  Expanded buildKey(MaterialColor color, noteNumber) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(noteNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.yellow, 2),
+              buildKey(Colors.purple, 3),
+              buildKey(Colors.lightGreen, 4),
+              buildKey(Colors.lightBlue, 7),
+              buildKey(Colors.deepOrange, 5),
+              buildKey(Colors.blueGrey, 6),
+            ],
+          ),
         ),
       ),
     );
